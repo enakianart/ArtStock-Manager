d554de816e498e22d1bda2073a3f51112fa66a89
Yorka Munchuli <ymunchuli@gmail.com>
2026-05-11 17:58:17 -0400

    fix-data modificado

--- fix-data.js
+++ fix-data.js
@@ -2,7 +2,7 @@ const sequelize = require('./src/config/database');
 async function fix() {
     try {
         // Cambiamos 'personaje' por 0 para que la gráfica pueda leerlo
-        await sequelize.query("UPDATE Productos SET subcategoria_personaje = '0' WHERE subcategoria_personaje = 'personaje'");
+        await sequelize.query("UPDATE Productos SET subcategoria_personaje = '0' WHERE subcategoria_personaje  = 'personaje'");
         console.log("✅ Datos corregidos correctamente.");
     } catch (e) { console.log("❌ Error:", e); }
     process.exit();
